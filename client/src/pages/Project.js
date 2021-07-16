a9be8d05a530f94cb79ac8d9259f14e4a0abab16
samrobers <sam.roberson32@gmail.com>
2021-07-16 15:51:29 -0600

    finished basic structure of project page

--- client/src/pages/Project.js
+++ client/src/pages/Project.js
@@ -4,19 +4,78 @@ import Logo from "./images/weatherdash.PNG";
 function Project() {
   return (
     <div>
-      <div className="card" style={{ width: 288 }}>
-        <img src={Logo} className="card-img-top" alt="..." />
-        <div className="card-body">
-          <h5 className="card-title">Card title</h5>
-          <p className="card-text">
-            Some quick example text to build on the card title and make up the
-            bulk of the card's content.
-          </p>
-          <a href="./" className="btn btn-primary">
-            Go somewhere
-          </a>
-        </div>
-      </div>
+      <main class="text-white">
+        <section class="row mt-5">
+          <section class="row mt-5">
+            <section className="row mt-5 text-center justify-content-center">
+              <div
+                className="card mx-4 project-card2 text-color p-3 shadow-lg"
+                style={{ width: 288 }}
+              >
+                <img src={Logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="./" className="btn mx-1 btn-outline-light">
+                    Source Code
+                  </a>
+                </div>
+              </div>
+              <div
+                className="card mx-4 project-card2 p-3 shadow-lg"
+                style={{ width: 288 }}
+              >
+                <img src={Logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="./" className="btn btn-outline-light">
+                    Source Code
+                  </a>
+                </div>
+              </div>
+              <div
+                className="card mx-4 project-card2 p-3 shadow-lg"
+                style={{ width: 288 }}
+              >
+                <img src={Logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="./" className="btn btn-outline-light">
+                    Source Code
+                  </a>
+                </div>
+              </div>
+              <div
+                className="card mx-4 project-card2 p-3 shadow-lg"
+                style={{ width: 288 }}
+              >
+                <img src={Logo} className="card-img-top" alt="..." />
+                <div className="card-body">
+                  <h5 className="card-title">Card title</h5>
+                  <p className="card-text">
+                    Some quick example text to build on the card title and make
+                    up the bulk of the card's content.
+                  </p>
+                  <a href="./" className="btn btn-outline-light">
+                    Source Code
+                  </a>
+                </div>
+              </div>
+            </section>
+          </section>
+        </section>
+      </main>
     </div>
   );
 }
